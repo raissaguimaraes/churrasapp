2451c5e7da4018153ac87474f05e2817c41cee30
raissaguimaraes <raissaguimaraes@live.com>
2022-08-04 12:55:47 -0300

    Trinca Logo

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,4 +1,4 @@
-import { Grid, createTheme, ThemeProvider } from "@mui/material";
+import { Grid, createTheme, ThemeProvider, Typography, Box, CardMedia } from "@mui/material";
 import Head from "next/head";
 
 const theme = createTheme({
@@ -23,6 +23,7 @@ const theme = createTheme({
 
 export function Layout({children}){
     return(
+      <>
         <Grid sx={{backgroundColor: "#FFD836", backgroundImage: 'url(/bbq_background.png)', backgroundRepeat: 'repeat-x', wrap: "wrap", width: '100%', height: "100vh"}}>
           <Head>
             <title>Agenda de Churras</title>
@@ -30,8 +31,20 @@ export function Layout({children}){
             <link rel="icon" href="/icon_bbq.svg" />
           </Head>
           <ThemeProvider theme={theme}>
+          <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography fontSize="32px" fontWeight="800" sx={{ maxWidth: "298px", marginBottom: "48px", marginTop: "70px" }}>
+            Agenda de Churras
+          </Typography>
             {children}
+          </Box>
           </ThemeProvider>
         </Grid>
+        </>
     )
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,25 +1,20 @@
-import Container from '@mui/material/Container';
 import Form from '../components/organisms/form';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { CardMedia } from '@mui/material';
 
 
 export default function Login(){
     return(
     <Box
         sx={{
+          marginTop: "54px",
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}
       >
-      <Paper />
-      
-      <Typography fontSize="32px" fontWeight="800" sx={{ maxWidth: "298px", marginBottom: "102px", marginTop: "70px" }}>
-        Agenda de Churras
-      </Typography>
       <Form />
+      <CardMedia component="img"  image="/trinca.svg" sx={{ height:"48px", width: "48px",  marginTop: "120px"}}  />
     </Box>
     )
 }
